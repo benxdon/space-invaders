e9a1dcc24c3b1e1bc200a28784b5a47e00c07954
benxdon <brandonlin2006@gmail.com>
2026-06-24 17:47:14 +0800

    added enemies but no logic yet

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,19 @@ type Size = {
   h: number,
 };
 
+type Player = Location & Size & {lives: number};
+type Enemy = Location & Size & {alive: boolean};
+
 // intializing game canvas variables
 const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d")!;
+if (!ctx){
+  throw new Error("2d canvas not found");
+};
 canvas.width = 600, canvas.height = 600;
 
 // initializing game objects variables
-const player: Location & Size = {x: canvas.width/2-5, y: canvas.height-30, w: 20, h: 10};
+const player: Player = {x: canvas.width/2-5, y: canvas.height-30, w: 20, h: 10, lives: 3};
 const keys: Record<string,boolean> = {
   "KeyA": false,
   "KeyD": false,
@@ -27,8 +33,27 @@ const keys: Record<string,boolean> = {
 };
 let bullets: Location[] = [];
 const playerSpeed = 400; // pixels per seconds
+const bulletSpeed = 200;
 let lastTime = 0;
 let spaceWasDown = false;
+
+const enemies: Enemy[] = [];
+const ENEMY_W = 30, ENEMY_H = 20;
+const ROWS = 5, COLS = 8;
+const OFFSET_X = 40, OFFSET_Y = 40; // margin
+const GAP = 15; // space between enemies
+
+for (let row = 0; row < ROWS; row++){
+  for (let col = 0; col < COLS; col++){
+    enemies.push({
+      x: OFFSET_X + col * (ENEMY_W + GAP),
+      y: OFFSET_Y + row * (ENEMY_H + GAP),
+      w: ENEMY_W,
+      h: ENEMY_H,
+      alive: true,
+    });
+  }
+}
 
 function recordDown(e:KeyboardEvent){
   keys[e.code] = true;
@@ -45,10 +70,6 @@ function gameLoop(currentTime: number){
   const delta = (currentTime - lastTime) / 1000; // pixels per second
   lastTime = currentTime;
   const spaceDown = keys["Space"];
-
-  if (!ctx){
-    throw new Error("2d canvas not found");
-  };
 
   ctx.clearRect(0 , 0, canvas.width, canvas.height);
 
@@ -69,10 +90,17 @@ function gameLoop(currentTime: number){
 
   for (let bullet of bullets) {
     ctx.fillRect(bullet.x, bullet.y,2, 5);
-    bullet.y -= 1;
+    bullet.y -= bulletSpeed * delta;
   }
 
   bullets = bullets.filter((bullet) => bullet.y + 5 > 0);
+
+  ctx.fillStyle = "lime";
+  for (let enemy of enemies){
+    if (enemy.alive){
+      ctx.fillRect(enemy.x, enemy.y, enemy.w, enemy.h);
+    }
+  }
 
   requestAnimationFrame(gameLoop);
 };
